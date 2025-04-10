e6530d48f71b84d13add5598c9978eda2fb2d341
gaddisai <saikrishna.gaddii@gmail.com>
2025-04-10 15:31:40 +0530

    first commit

---
+++ qr1/q1.js
@@ -0,0 +1,6 @@
+type mongosh in command prompt 
+show dbs;
+use mydb;
+show collections;
+db.createCollections("products")
+show collections;
---
+++ qr1/q2.js
@@ -0,0 +1,5 @@
+db.products.insertOne([
+    { name: "Laptop", price: 1000 },
+    { name: "Smartphone", price: 500 },
+    { name: "Tablet", price: 300 }
+])
