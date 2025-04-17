30642985e5dfcc3a68e600aa96956ad1ee7e2886
gaddisai <saikrishna.gaddii@gmail.com>
2025-04-17 12:23:32 +0530

    changes done

--- qr1/q2.js
+++ qr1/q2.js
@@ -47,6 +47,13 @@ db.Users.find({email:"deepesh@gmail.com"},{_id: false,name: true})
 //delete all the users from the database
 db.Users.deleteMany({})
 
+//It deletes all the names which contains the name:test1
+db.Users.deleteMany({name:"test1"})
+
+//It deletes only the startring name of the name:test1 and deletes only one 
+db.Users.deleteOne({name:"test2"})
+
+//
 db.employees.insertOne({
     name:"deepesh",
     email: 'deepesh@gmail.com',
@@ -64,3 +71,33 @@ db.employees.find({},{_id:0,name:true,salary:true}).sort({salary:1})
 
 //to display the employee whose city is medchal
 db.employees.find({'address.city':'Medchal'})
+
+
+//to display the employees whose salary is equal to 7000
+db.employees.find({salary:{$eq:7000}})
+
+//to display the employees whose salary is greater than 7000
+db.employees.find({salary:{$gt:7000}})
+
+//to display the employees whose salary is less than 7000
+db.employees.find({salary:{$lt:7000}})
+
+//
+db.employees.find({salary:{$gte:3000},department:'CSE'})
+
+//we are keeping 'or' operator to find the employees whose salary is greater than 4000 or department is CSE
+db.employees.find({$or:[{salary:{$gte:4000}},{department:'CSE'}]})
+
+//we are keeping 'and' operator to find the employees whose salary is greater than 4000 and department is CSE
+db.employees.find({$and:[{salary:{$gte:4000}},{department:'CSE'}]})
+
+
+//display the employees whose department is not CSE
+db.employees.find({$nor:[{department:'CSE'}]})
+
+
+//display the employee details who do not have the department field
+db.employees.find({department:{$exists:false}})
+
+//update the employee whose email is sai@gmail.com to update the salary to 6000
+db.employees.updateOne({email:'sai@gmail.com'},{$set:{salary:6000}})
