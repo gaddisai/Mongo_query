1bbd55e5ef3a9c89ff2ea62d3f393923fc93c200
gaddisai <saikrishna.gaddii@gmail.com>
2025-04-22 10:43:33 +0530

    changes done

---
+++ qr1/Initial_Quries.js
@@ -0,0 +1,11 @@
+//starting step after opening cmd 
+type mongosh in command prompt 
+
+// and than click the below command
+use mydb;
+
+//for creating of tables
+db.createCollections("products")
+
+//for showing the tables
+show collections;
